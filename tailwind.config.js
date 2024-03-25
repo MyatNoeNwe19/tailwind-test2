7bbb71ebe6246a28f125dc9bf8ea54cc8cae299b
MyatNoeNwe19 <maythu314279@gmail.com>
2024-03-25 23:34:05 +0630

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'padauk': '#fcba03',
+        'kyaeene':'#b5640d',
+        
+      }
+    },
+    fontFamily:{
+      'monsieur':["Monsieur", "cursive"]
+    },
+
+  },
+  plugins: [
+
+  ],
+}
+
